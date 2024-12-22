7fe7e46384bc0bae236d9aafe102e006a172974f
Александра <aleksandrasilina@gmail.com>
2024-12-22 16:22:18 +0300

    add drf-yasg documentation

--- config/settings.py
+++ config/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_yasg',
     "workers",
     "users",
 ]
@@ -81,7 +82,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
