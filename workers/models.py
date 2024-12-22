4c53cc4664156608017fd790f4f79455e3e612ff
Александра <aleksandrasilina@gmail.com>
2024-12-22 16:21:44 +0300

    change workers/models.py

--- workers/models.py
+++ workers/models.py
@@ -20,13 +20,21 @@ class Worker(models.Model):
             "прораб",
         ),
     )
-
+    worker_id = models.PositiveSmallIntegerField(verbose_name="ID работника",
+                                                 help_text="Укажите ID работника")
     name = models.CharField(max_length=100, verbose_name="ФИО",
                             help_text="Укажите ФИО")
-    team_number = models.PositiveSmallIntegerField(verbose_name="Номер бригады",
-                                                   help_text="Укажите номер бригады")
+    team_id = models.PositiveSmallIntegerField(verbose_name="ID бригады",
+                                               help_text="Укажите ID бригады")
     salary = models.PositiveIntegerField(verbose_name="Зарплата",
                                          help_text="Укажите зарплату")
     specialization = models.CharField(max_length=20, choices=WORKER_SPECIALIZATION_CHOICES,
                                       verbose_name="Специализация",
                                       help_text="Укажите специализацию")
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = "Работник"
+        verbose_name_plural = "Работники"
